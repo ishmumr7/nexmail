28c40823779782a37c95afdb0c77748ff1d6a904
Ishmum <ishmum20000@gmail.com>
2024-08-17 20:36:40 +0800

    Format code

--- src/shared/utils/Providers.tsx
+++ src/shared/utils/Providers.tsx
@@ -4,24 +4,29 @@ import { NextUIProvider } from "@nextui-org/react";
 import { usePathname } from "next/navigation";
 
 interface ProviderProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export default function Providers({ children }: ProviderProps) {
-  const pathname = usePathname();
-  const { isLoaded } = useUser();
+	const pathname = usePathname();
+	const { isLoaded } = useUser();
 
-  if(!isLoaded) return null;
+	if (!isLoaded) return null;
 
-  return (
-    <NextUIProvider>
-      {pathname !== "/dashboard/new-email" && pathname !== "/" && pathname !== "/sign-up" && pathname !== "/subscribe" && pathname !== "/sign-in" ? (
-        <div className="w-full flex">
-          <div className="w-[290px] h-screen overflow-y-scroll"></div>
-        </div>
-      ) : (
-        <>{children}</>
-      ) }
-    </NextUIProvider>
-  )
+	return (
+		<NextUIProvider>
+			{pathname !== "/dashboard/new-email" &&
+			pathname !== "/" &&
+			pathname !== "/sign-up" &&
+			pathname !== "/subscribe" &&
+			pathname !== "/sign-in" ? (
+				<div className="w-full flex">
+					<div className="w-[290px] h-screen overflow-y-scroll"></div>
+					{children}
+				</div>
+			) : (
+				<>{children}</>
+			)}
+		</NextUIProvider>
+	);
 }
