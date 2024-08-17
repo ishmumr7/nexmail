4e35944239904de661dfe58aaacb7d8184be7c70
Ishmum <ishmum20000@gmail.com>
2024-08-17 16:05:20 +0800

    Complete footer

--- src/shared/widgets/footer/footer.tsx
+++ src/shared/widgets/footer/footer.tsx
@@ -10,10 +10,10 @@ const Footer = () => {
 					<Link href="/">
 						<FooterLogo />
 					</Link>
-					<p className="text-2xl py-4">
+					<p className="text-2xl pt-8">
 						Get Nexmail updates delivered directly to your inbox.
 					</p>
-					<div className="flex items-center w-[350px] py-2">
+					<div className="flex items-center w-[350px] pt-8">
 						<input
 							type="email"
 							name="email"
@@ -25,26 +25,37 @@ const Footer = () => {
 							Subscribe
 						</button>
 					</div>
-					<p className="py-2 text-xs">
+					<p className="py-6 text-xs">
 						By subscribing you agree to our Privacy Policy and provide consent
 						to receive updates from our company.
 					</p>
 				</div>
-        <div className="w-full md:w-[60%] flex md:justify-end justify-center py-5 md:py-0">
-          <div className="md:w-[50%] flex justify-around">
-            <div>
-              <ul>
-                <li className="text-xl pb-4 cursor-pointer">
-                  Create
-                </li>
-                <li className="text-xl pb-4 cursor-pointer">
-                  Write
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+				<div className="w-full md:w-[60%] flex md:justify-end justify-center py-5 md:py-0">
+					<div className="md:w-[50%] flex justify-around">
+						<div>
+							<ul>
+								<li className="text-xl pb-4 cursor-pointer">Create</li>
+								<li className="text-xl pb-4 cursor-pointer">Write</li>
+								<li className="text-xl pb-4 cursor-pointer">Grow</li>
+								<li className="text-xl pb-4 cursor-pointer">Monetize</li>
+								<li className="text-xl pb-4 cursor-pointer">Analyze</li>
+							</ul>
+						</div>
+						<div>
+							<ul>
+								<li className="text-xl pb-4 cursor-pointer">Careers</li>
+								<li className="text-xl pb-4 cursor-pointer">Pricing</li>
+								<li className="text-xl pb-4 cursor-pointer">Shop</li>
+								<li className="text-xl pb-4 cursor-pointer">Compare</li>
+								<li className="text-xl pb-4 cursor-pointer">Love</li>
+							</ul>
+						</div>
+					</div>
+				</div>
 			</div>
+			<p className="text-lg text-center pb-10">
+				&copy; 2024 Nexmail. Inc. All rights reserved.
+			</p>
 		</footer>
 	);
 };
