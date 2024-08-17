7e4279036f3b57ee3ad0873f9d3aec813d4cf362
Ishmum <ishmum20000@gmail.com>
2024-08-17 23:21:45 +0800

    Set up dashboard files

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Dashboard from '@/modules/dashboard/dashboard'
+
+const Page = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
+}
+
+export default Page;
---
+++ src/modules/dashboard/dashboard.tsx
@@ -0,0 +1,9 @@
+import Main from './elements/main/main'
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Main />
+    </div>
+  )
+}
