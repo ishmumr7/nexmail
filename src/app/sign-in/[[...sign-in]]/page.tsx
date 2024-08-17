1617bff63a4332686f7b910fd83a05d8be93a292
Ishmum <ishmum20000@gmail.com>
2024-08-17 20:18:05 +0800

    Update style

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,10 @@
 "use client";
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn signUpUrl='/sign-up' />
+	return (
+		<div className="w-full flex h-screen items-center justify-center">
+			<SignIn signUpUrl="/sign-up" />
+		</div>
+	);
 }
