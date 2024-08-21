64619657aea22c6781f8b40b26c3977e42c75e0f
Ishmum <ishmum20000@gmail.com>
2024-08-20 23:40:33 -0900

    Format code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Home from "@/modules/home";
 import Image from "next/image";
 
-export default function Page () {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+export default function Page() {
+	return (
+		<div>
+			<Home />
+		</div>
+	);
 }
