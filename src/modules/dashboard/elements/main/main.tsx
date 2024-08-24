6e83544e660a2a552cca46396c739756df99a1ec
Ishmum <ishmum20000@gmail.com>
2024-08-24 15:21:27 +0800

    Add overview card and subscribers chart

--- src/modules/dashboard/elements/main/main.tsx
+++ src/modules/dashboard/elements/main/main.tsx
@@ -1,4 +1,6 @@
 "use client";
+import DashboardOverviewCard from "@/shared/components/cards/overview.card";
+import SubscribersChart from "@/shared/components/charts/subscribers.chart";
 import { useUser } from "@clerk/nextjs";
 
 const Main = () => {
@@ -9,6 +11,13 @@ const Main = () => {
 			<h1 className="text-2xl text-surface-900 font-medium">
 				Hi {user?.fullName}👋
 			</h1>
+			<p className="opacity-[0.7] text-base text-[rgb(107,114,128)] font-[500] mt-[0.25rem]">Here&apos;s how your publication is doing</p>
+			<div className="w-full flex">
+				<div className="w-[65%] min-h-[88vh] pr-5 mt-5">
+					<DashboardOverviewCard />
+					<SubscribersChart />
+				</div>
+			</div>
 		</div>
 	);
 };
